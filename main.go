72ea737710c6505b2bad7acbf5b7ae2ee4fec2fb
Karine Valenca <valenca.karine@gmail.com>
2020-10-17 15:32:36 -0300

    add webpage struct and templates

--- main.go
+++ main.go
@@ -8,12 +8,31 @@ import (
 	"golang.org/x/net/html"
 	"bytes"
 	"io"
+	"html/template"
 )
 
+type WebPage struct {
+	HTMLVersion string
+	PageTitle string
+	Headings []string
+	CounterInternalLinks int
+	CounterExternalLinks int
+	CounterInaccessibleLinks int
+	ContainsLoginForm bool
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 func main()  {
+	
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
+
+
 
 func index(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("https://golang.org/")
@@ -21,32 +40,41 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	
-	getHtmlElement(resp, "h2")
-	fmt.Fprintf(w, "Hello world")
+	var webpage WebPage
+	webpage.PageTitle = getHtmlElement(resp, "title")[0] //todo
+ 	fmt.Println(webpage.PageTitle)
+	tpl.ExecuteTemplate(w, "index.gohtml", webpage)
 }
 
 
-func getHtmlElement(resp *http.Response, htmlElement string) {
+func getHtmlElement(resp *http.Response, htmlElement string) []string {
+	//converts http.Response.Body to *html.Node
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 	doc, _ := html.Parse(strings.NewReader(string(body)))
+
+	//get html element
 	var element *html.Node
+	var stringElements []string
 	var f func(*html.Node)
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == htmlElement {
 			element = n
-			formatHtml(element)
+			stringElements = append(stringElements, formatHtml(element))
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
 		}
 	}
 	f(doc)
+	return stringElements
 }
 
-func formatHtml(element *html.Node) {
+
+func formatHtml(element *html.Node) string{
 	var buffer bytes.Buffer
 	w := io.Writer(&buffer)
 	html.Render(w, element)
-	fmt.Println(buffer.String())
+	return buffer.String()
 }
